0f33ecf8a8aad3cf7d4a415379cc6a386a51b442
MHayes <mhayes0022@gmail.com>
2024-03-03 14:51:28 -0600

    finally, finally have working API call to get temp

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,33 +1,30 @@
-
 const APIkey = 'caedc3880dded376b8777ec4072a1cca';
 let city = 'Paris';
-let coordinates;
-let latitude;
-let longitude;
 
 
 function getWeather() {
     let translateWeatherURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIkey}`
     fetch(translateWeatherURL)
-        .then(response => {
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
             latitude = data[0].lat;
             longitude = data[0].lon;
-            let openWeatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIkey}`;
-            fetch(openWeatherURL)
-                .then(response => {
-                    return console.log(response.json());
-                })
-        })
 
-    // .then(data => {
-    //     console.log(data);
-    //     .catch(error => {
-    //         console.error('There is an Error', error);
-    //     });
-    // })
+            let openWeatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIkey}`;
+            return fetch(openWeatherURL)
+        })
+        .then (response => response.json())
+        .then(data => {
+            let currentTemp = data.list[0].main.temp;
+            console.log(currentTemp);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 
 };
 getWeather();
+
+
+
+//$('#temperature').textContent = currentTemp;
