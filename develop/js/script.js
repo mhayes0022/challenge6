fa265f1533d2b541ddfb5d9a1dabebff763874b8
MHayes <mhayes0022@gmail.com>
2024-03-17 18:32:19 -0500

    added code to handle if user enters a non valid city

--- develop/js/script.js
+++ develop/js/script.js
@@ -42,25 +42,16 @@ let forecastFiveTemperature = document.getElementById('tempDayFive');
 let forecastFiveWind = document.getElementById('windDayFive');
 let forecastFiveHumidity = document.getElementById('humidityDayFive');
 
-//Below collects the city the user has searched
+
+//Below collects the city the user has searched and ensures an empty search box wasn't submitted
 function collectUserInput() {
     let city = searchInput.value.trim();
-    if (city == '' || city == null){
+    if (city == '' || city == null) {
         alert("You must enter a city")
         return;
     }
-    //checkCity(city)
     getLocationCoordinates(city)
 };
-
-//Can I stick the check to ensure the city is valid in the function above? Below?
-
-// function checkCity(city) {
-//     if (city ==) 
-   
-// };
-
-// == "HTTP/1.1 404 Not Found"
 
 //Below fetches API data as longitude and latitude 
 function getLocationCoordinates(city) {
@@ -68,6 +59,11 @@ function getLocationCoordinates(city) {
     fetch(translateWeatherURL)
         .then(response => response.json())
         .then(data => {
+            //checks that the user submitted city is valid
+            if (data.length === 0) {
+                alert("Please enter a valid city")
+                return;
+            }
             let latitude = data[0].lat;
             let longitude = data[0].lon;
 
@@ -76,6 +72,28 @@ function getLocationCoordinates(city) {
         })
         .catch(error => {
             console.error('Error fetching city coordinates', error);
+        });
+};
+
+
+//Below gets the current weather from the Open Weather API
+function getCurrentWeather(lat, lon) {
+    let openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${APIkey}`;
+
+    fetch(openWeatherURL).then((res) => res.json())
+        .then(data => {
+            storeInLocalStorage(data.name)
+            cityName.innerHTML = data.name;
+            currentDate.innerHTML = dayjs.unix(data.dt).format('MMMM DD YYYY');
+            currentTemp.innerHTML = 'Temperature: ' + data.main.temp + ' F';
+            currentWind.innerHTML = 'Wind Speed: ' + data.wind.speed + ' MPH';
+            currentHumid.innerHTML = 'Humidity: ' + data.main.humidity + ' %';
+            let iconId = data.weather[0].icon;
+            let weatherIcon = `https://openweathermap.org/img/wn/${iconId}@2x.png`
+            currentWeatherIcon.setAttribute('src', weatherIcon);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
         });
 };
 
@@ -89,7 +107,7 @@ function getFiveDayForecast(lat, lon) {
             //forecast information for tomorrow
             let unixDateOne = data.list[8].dt;
             let iconIdOne = data.list[8].weather[0].icon;
-            let weatherIconOne = `https://openweathermap.org/img/wn/${iconIdOne}@2x.png`
+            let weatherIconOne = `https://openweathermap.org/img/wn/${iconIdOne}@2x.png`;
             forecastOneIcon.setAttribute('src', weatherIconOne);
             forecastOneTemperature.innerHTML = 'Temperature: ' + data.list[8].main.temp + ' F';
             forecastOneWind.innerHTML = 'Wind Speed: ' + data.list[8].wind.speed + ' MPH';
@@ -98,7 +116,7 @@ function getFiveDayForecast(lat, lon) {
             //forecast information for the day after tomorrow
             let unixDateTwo = data.list[16].dt;
             let iconIdTwo = data.list[16].weather[0].icon;
-            let weatherIconTwo = `https://openweathermap.org/img/wn/${iconIdTwo}@2x.png`
+            let weatherIconTwo = `https://openweathermap.org/img/wn/${iconIdTwo}@2x.png`;
             forecastTwoIcon.setAttribute('src', weatherIconTwo);
             forecastTwoTemperature.innerHTML = 'Temperature: ' + data.list[16].main.temp + ' F';
             forecastTwoWind.innerHTML = 'Wind Speed: ' + data.list[16].wind.speed + ' MPH';
@@ -107,7 +125,7 @@ function getFiveDayForecast(lat, lon) {
             //forecast information for three days from now
             let unixDateThree = data.list[24].dt;
             let iconIdThree = data.list[24].weather[0].icon;
-            let weatherIconThree = `https://openweathermap.org/img/wn/${iconIdThree}@2x.png`
+            let weatherIconThree = `https://openweathermap.org/img/wn/${iconIdThree}@2x.png`;
             forecastThreeIcon.setAttribute('src', weatherIconThree);
             forecastThreeTemperature.innerHTML = 'Temperature: ' + data.list[24].main.temp + ' F';
             forecastThreeWind.innerHTML = 'Wind Speed: ' + data.list[24].wind.speed + ' MPH';
@@ -116,7 +134,7 @@ function getFiveDayForecast(lat, lon) {
             //forecast information for four days from now
             let unixDateFour = data.list[32].dt;
             let iconIdFour = data.list[32].weather[0].icon;
-            let weatherIconFour = `https://openweathermap.org/img/wn/${iconIdFour}@2x.png`
+            let weatherIconFour = `https://openweathermap.org/img/wn/${iconIdFour}@2x.png`;
             forecastFourIcon.setAttribute('src', weatherIconFour);
             forecastFourTemperature.innerHTML = 'Temperature: ' + data.list[32].main.temp + ' F';
             forecastFourWind.innerHTML = 'Wind Speed: ' + data.list[32].wind.speed + 'MPH';
@@ -125,56 +143,28 @@ function getFiveDayForecast(lat, lon) {
             //forecast information for five days from now
             let unixDateFive = data.list[39].dt;
             let iconIdFive = data.list[39].weather[0].icon;
-            let weatherIconFive = `https://openweathermap.org/img/wn/${iconIdFive}@2x.png`
+            let weatherIconFive = `https://openweathermap.org/img/wn/${iconIdFive}@2x.png`;
             forecastFiveIcon.setAttribute('src', weatherIconFive);
             forecastFiveTemperature.innerHTML = 'Temperature: ' + data.list[39].main.temp + ' F';
             forecastFiveWind.innerHTML = 'Wind Speed: ' + data.list[39].wind.speed + ' MPH';
             forecastFiveHumidity.innerHTML = 'Humidity: ' + data.list[39].main.humidity + ' %';
 
             convertDates(unixDateOne, unixDateTwo, unixDateThree, unixDateFour, unixDateFive);
-            //console.log(data.list);
         })
         .catch(error => {
             console.error('Error fetching weather', error);
         });
-}
+};
 
-//Below translates the dates of the five day weather forecast from unix to month, day format
+
+//Below translates the dates of the five day weather forecast from unix to month,day format
 function convertDates(unixDateOne, unixDateTwo, unixDateThree, unixDateFour, unixDateFive) {
     forecastDateOne.innerHTML = dayjs.unix(unixDateOne).format('MMMM DD');
     forecastDateTwo.innerHTML = dayjs.unix(unixDateTwo).format('MMMM DD');
     forecastDateThree.innerHTML = dayjs.unix(unixDateThree).format('MMMM DD');
     forecastDateFour.innerHTML = dayjs.unix(unixDateFour).format('MMMM DD');
     forecastDateFive.innerHTML = dayjs.unix(unixDateFive).format('MMMM DD');
-}
-
-
-
-
-//Below gets the current weather from the Open Weather API
-function getCurrentWeather(lat, lon) {
-    let openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${APIkey}`;
-
-    fetch(openWeatherURL).then((res) => res.json())
-        .then(data => {
-            storeInLocalStorage(data.name)
-            //console.log(data);
-            cityName.innerHTML = data.name;
-            //let currentUnixDate = data.dt;
-            currentDate.innerHTML = dayjs.unix(data.dt).format('MMM DD YYYY');
-            currentTemp.innerHTML = 'Temperature: ' + data.main.temp + ' F';
-            currentWind.innerHTML = 'Wind Speed: ' + data.wind.speed + ' MPH';
-            currentHumid.innerHTML = 'Humidity: ' + data.main.humidity + ' %';
-            let iconId = data.weather[0].icon;
-            let weatherIcon = `https://openweathermap.org/img/wn/${iconId}@2x.png`
-            currentWeatherIcon.setAttribute('src', weatherIcon);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-}
-
-
+};
 
 //Below wraps together the necessary functions that follow the 'search' button being clicked
 function searchCity(event) {
@@ -217,12 +207,8 @@ searchHistoryBtns();
 
 
 // Start at end of history array and count down to show the most recent at the top.
-// If there is no search term return the function
 // First filters through all of the data and returns only data that falls between one day after the current data and up to 5 days later.
 // Then filters through the data and returns only data captured at noon for each day.
-// Don't continue if there is nothing in the search form
 
 //TO DO:
 //what hours do I set the forecast to get? Is there a way to have it filter to always retrieve noon?
-//display today's date at top of current forecast
-//contingency for if a non city is searched
